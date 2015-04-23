4e86ef763732d101c1fe35ca7e284bfb88a38709
abhivendra <abhivendrasingh@gmail.com>
2015-04-23 16:40:59 +0530

    initial

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php 
+include "app/config.php";
+include "app/detect.php";
+
+if ($page_name=='') {
+	include $browser_t.'/index.html';
+	}
+elseif ($page_name=='index.html') {
+	include $browser_t.'/index.html';
+	}
+elseif ($page_name=='about.html') {
+	include $browser_t.'/about.html';
+	}
+elseif ($page_name=='blog.html') {
+	include $browser_t.'/blog.html';
+	}
+elseif ($page_name=='bsingle.html') {
+	include $browser_t.'/bsingle.html';
+	}
+elseif ($page_name=='icons.html') {
+	include $browser_t.'/icons.html';
+	}
+elseif ($page_name=='iconsingle.html') {
+	include $browser_t.'/iconsingle.html';
+	}
+elseif ($page_name=='contact.html') {
+	include $browser_t.'/contact.html';
+	}
+elseif ($page_name=='contact-post.html') {
+	include $browser_t.'/contact.html';
+	include 'app/contact.php';
+	}
+else
+	{
+		include $browser_t.'/404.html';
+	}
+
+?>
